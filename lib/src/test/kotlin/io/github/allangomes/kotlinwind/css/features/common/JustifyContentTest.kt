62e39593d3c3a1eeff08f35bff95d00ef572e1b9
Allan Gomes <allanmaiagomes@gmail.com>
2024-08-19 18:57:59 -0300

    Fix tests

--- lib/src/test/kotlin/io/github/allangomes/kotlinwind/css/features/common/JustifyContentTest.kt
+++ lib/src/test/kotlin/io/github/allangomes/kotlinwind/css/features/common/JustifyContentTest.kt
@@ -10,63 +10,63 @@ class JustifyContentTest {
 
   @Test
   fun justify_normal() {
-    val expected = Style(JUSTIFY_CONTENT,"justify-normal").toString()
+    val expected = Style(JUSTIFY_CONTENT,"normal").toString()
     val returned = KW.inline { grid.justify_normal }
     assertEquals(expected, returned)
   }
 
   @Test
   fun justify_start() {
-    val expected = Style(JUSTIFY_CONTENT,"justify-start").toString()
+    val expected = Style(JUSTIFY_CONTENT,"flex-start").toString()
     val returned = KW.inline { grid.justify_start }
     assertEquals(expected, returned)
   }
 
   @Test
   fun justify_end() {
-    val expected = Style(JUSTIFY_CONTENT,"justify-end").toString()
+    val expected = Style(JUSTIFY_CONTENT,"flex-end").toString()
     val returned = KW.inline { grid.justify_end }
     assertEquals(expected, returned)
   }
 
   @Test
   fun justify_center() {
-    val expected = Style(JUSTIFY_CONTENT,"justify-center").toString()
+    val expected = Style(JUSTIFY_CONTENT,"center").toString()
     val returned = KW.inline { grid.justify_center }
     assertEquals(expected, returned)
   }
 
   @Test
   fun justify_between() {
-    val expected = Style(JUSTIFY_CONTENT,"justify-between").toString()
+    val expected = Style(JUSTIFY_CONTENT,"space-between").toString()
     val returned = KW.inline { grid.justify_between }
     assertEquals(expected, returned)
   }
 
   @Test
   fun justify_around() {
-    val expected = Style(JUSTIFY_CONTENT,"justify-around").toString()
+    val expected = Style(JUSTIFY_CONTENT,"space-around").toString()
     val returned = KW.inline { grid.justify_around }
     assertEquals(expected, returned)
   }
 
   @Test
   fun justify_evenly() {
-    val expected = Style(JUSTIFY_CONTENT,"justify-evenly").toString()
+    val expected = Style(JUSTIFY_CONTENT,"space-evenly").toString()
     val returned = KW.inline { grid.justify_evenly }
     assertEquals(expected, returned)
   }
 
   @Test
   fun justify_stretch() {
-    val expected = Style(JUSTIFY_CONTENT,"justify-stretch").toString()
+    val expected = Style(JUSTIFY_CONTENT,"stretch").toString()
     val returned = KW.inline { grid.justify_stretch }
     assertEquals(expected, returned)
   }
 
   @Test
   fun flex_support() {
-    val expected = Style(JUSTIFY_CONTENT,"justify-normal").toString()
+    val expected = Style(JUSTIFY_CONTENT,"normal").toString()
     val returned = KW.inline { flex.justify_normal }
     assertEquals(expected, returned)
   }
